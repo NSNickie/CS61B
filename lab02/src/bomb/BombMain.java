7bac68f9c06a0b5f7effeca9e203c1fc0221ff76
NSNick <nsmitnick@gmail.com>
2024-01-11 22:01:32 +0900

    lab 02

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,20 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0,9,3,0,8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            // Add 1336 words (to make i=1337 at the 1337th position)
+            for (int i = 0; i < 1337; i++) {
+                passwordBuilder.append("random ");
+            }
+            passwordBuilder.append("-81201430");
+            b.phase2(passwordBuilder.toString());
         }
     }
 }
