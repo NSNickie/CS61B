a42dd7760a338a70bea4e77140a5f9419cf82bbf
NSNick <nsmitnick@gmail.com>
2024-05-21 18:29:23 +0900

    project2b done #3

---
+++ proj2b/tests/TestMultiWordK5Hyponyms.java
@@ -0,0 +1,27 @@
+import browser.NgordnetQuery;
+import browser.NgordnetQueryHandler;
+import main.AutograderBuddy;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class TestMultiWordK5Hyponyms {
+    public static final String WORDS_FILE = "data/ngrams/top_49887_words.csv";
+    public static final String TOTAL_COUNTS_FILE = "data/ngrams/total_counts.csv";
+    public static final String SMALL_SYNSET_FILE = "data/wordnet/synsets.txt";
+    public static final String SMALL_HYPONYM_FILE = "data/wordnet/hyponyms.txt";
+
+    @Test
+    public void testFoodCakeK5() {
+        NgordnetQueryHandler studentHandler = AutograderBuddy.getHyponymHandler(
+                WORDS_FILE, TOTAL_COUNTS_FILE, SMALL_SYNSET_FILE, SMALL_HYPONYM_FILE);
+        List<String> words = List.of("food,cake");
+
+        NgordnetQuery nq = new NgordnetQuery(words, 1950, 1990, 5);
+        String actual = studentHandler.handle(nq);
+        String expected = "[biscuit, cake, kiss, snap, wafer]";
+        assertThat(actual).isEqualTo(expected);
+    }
+}
